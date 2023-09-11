40c718e66bc2263206c1acbef107b86605552fe8
Tarzeron <tarzeron@gmail.com>
2023-09-11 11:09:50 +0300

    wallet connect

--- src/app/test-page/page.tsx
+++ src/app/test-page/page.tsx
@@ -1,8 +1,25 @@
 'use client';
 import {useState} from "react";
+import Wallet from "../blockchain/wallet";
 
 const TestPage: React.FC = () => {
-    const {stakeAmount, setStakeAmount} = useState();
+    const [wallet, setWallet] = useState<any>(undefined);
+    const [stakeAmount, setStakeAmount] = useState("");
+    const [PHZ, setBalancePHZ] = useState(0);
+    const [phApt, setBalancephApt] = useState(0);
+    const [yPhApt, setBalanceyPhApt] = useState(0);
+    const [pPhApt, setBalancepPhApt] = useState(0);
+    const updateInfos = async () => {
+
+    };
+
+    const connectWallet = async () => {
+        console.log("wallet, setWallet", wallet, setWallet)
+        let w = new Wallet();
+        await w.connect();
+        setWallet(w);
+        await updateInfos();
+    };
     const stake = () => {
         console.log("stake");
     };
@@ -11,11 +28,17 @@ const TestPage: React.FC = () => {
     };
     return (
         <div style={{width: "800px", height: "1000px", backgroundColor: "#999", position: "absolute"}}>
-            <text>{"sdfsd"}</text>
+            <button style={{backgroundColor: "#555", margin: "10px"}} onClick={connectWallet}>connect wallet</button>
             <br/>
-            <text>{"sdfsd"}</text>
+            <text>address: {!!wallet ? wallet.getAccount() : "0x"}</text>
             <br/>
-            <text>{"sdfsd"}</text>
+            <text>balance PHZ: {PHZ}</text>
+            <br/>
+            <text>balance phApt: {phApt}</text>
+            <br/>
+            <text>balance yPhApt: {yPhApt}</text>
+            <br/>
+            <text>balance pPhApt: {pPhApt}</text>
             <br/>
             <text>stake amount:</text>
             <input type="text" value={stakeAmount}/> <br/>
