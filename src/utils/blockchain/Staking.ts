636571e1ff6c9a16f30004e9bde720091cb98cf9
Tarzeron <tarzeron@gmail.com>
2023-09-12 16:22:48 +0300

    requests for stake_ph_apt, withdraw_ph_apt to staking contract

--- src/utils/blockchain/Staking.ts
+++ src/utils/blockchain/Staking.ts
@@ -40,6 +40,42 @@ class Staking {
             return res.hash;
         });
     }
+
+    async stakePhApt(wallet, amount) {
+        const payload = {
+            type: "entry_function_payload",
+            function: `${this.contractAddress}::staking::stake_ph_apt`,
+            type_arguments: [],
+            arguments: [amount.toString()]
+        };
+        const options = {
+            max_gas_amount: "10000",
+            gas_unit_price: "100",
+            expiration_timestamp_secs: new Date().getTime().toString(),
+        };
+        console.log('payload', payload, "options", options);
+        return await wallet.signAndSubmitTransaction(payload, options).then(res => {
+            return res.hash;
+        });
+    }
+
+    async withdrawPhApt(wallet, amount) {
+        const payload = {
+            type: "entry_function_payload",
+            function: `${this.contractAddress}::staking::withdraw_ph_apt`,
+            type_arguments: [],
+            arguments: [amount.toString()]
+        };
+        const options = {
+            max_gas_amount: "10000",
+            gas_unit_price: "100",
+            expiration_timestamp_secs: new Date().getTime().toString(),
+        };
+        console.log('payload', payload, "options", options);
+        return await wallet.signAndSubmitTransaction(payload, options).then(res => {
+            return res.hash;
+        });
+    }
 }
 
 export default Staking
