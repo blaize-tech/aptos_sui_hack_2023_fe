e8bee6dd2eca5abb732ed81cf1ca4a0b1fc74876
Mark Tsyrulnyk <mark.ts@blaize.tech>
2023-09-13 00:57:32 +0300

    seo title

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 // Basic app settings
-export const APP_SEO_TITLE: string = 'Aptos App';
-export const APP_SEO_DESCRIPTION: string = 'Seo description ...';
+export const APP_SEO_TITLE: string = 'Phizen';
+export const APP_SEO_DESCRIPTION: string = 'Phizen yield protocol';
 export const APP_DOMAIN_URL: string = 'http://localhost:3000/';
