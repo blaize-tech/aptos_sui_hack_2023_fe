b32339d276634c3bb521ccb3f6835599d729a324
AlexanderOsadchyy <alex.o@blaize.tech>
2023-09-12 12:37:12 +0300

    Add pools / disable eslint

--- src/staticPages/Pools/Pools.tsx
+++ src/staticPages/Pools/Pools.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import {Box, Grid, Text} from '@chakra-ui/react';
 import { HtmlMeta } from '@look/components';
+import PoolsList from './components/pools-list/PoolsList';
+import {ConnectWalletButton} from "../../layout/components/ConnectWalletButton";
 
 export const Pools = () => {
   return (
     <Box>
       <HtmlMeta title="Pools" />
       <Box as="section">
-        <Text as="h1">Pools</Text>
+        <Grid templateColumns="1fr auto" gap="32px" alignItems="center" mb="40px">
+          <Box>
+            <Text as="h1" mb="24px">
+              Pools
+            </Text>
+            <Text color="gray" fontWeight={500}>
+              Tagline Will Goes Here
+            </Text>
+          </Box>
+          <ConnectWalletButton />
+        </Grid>
+        <PoolsList />
       </Box>
     </Box>
   );
