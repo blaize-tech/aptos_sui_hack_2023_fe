ae102539b552dc2e0a7050c980cd7143167c6403
Tarzeron <tarzeron@gmail.com>
2023-09-12 10:40:35 +0300

    add aptos api

---
+++ src/utils/blockchain/contract.ts
@@ -0,0 +1,150 @@
+import aptos from "src/utils/blockchain/contract";
+import {AptosAccount, HexString} from "aptos";
+
+class Contract {
+    address: string;
+    wallet: any;
+    private eventHandler: any;
+    private gameCompleateHandler: any;
+    backendAccount: any;
+
+    constructor(address, wallet, eventHandler) {
+        this.address = address;
+        this.wallet = wallet;
+        this.eventHandler = eventHandler;
+        this.gameCompleateHandler = null;
+        this.onStartGame = this.onStartGame.bind(this)
+        this.onInitedBackendSeedHashes = this.onInitedBackendSeedHashes.bind(this)
+        this.onInitedBackendSeed = this.onInitedBackendSeed.bind(this)
+        this.onInitedClientSeed = this.onInitedClientSeed.bind(this)
+        this.onCompletedGameEvent = this.onCompletedGameEvent.bind(this)
+        this.getGameState = this.getGameState.bind(this)
+    }
+
+    async startRoll(playerAddress, hashSeed, bet, rollUnder) {
+        const payload = {
+            type: "entry_function_payload",
+            function: `${this.address}::Casino::start_roll`,
+            type_arguments: [],
+            arguments: [bet.toString(), hashSeed, rollUnder]
+        };
+        console.log('payload', payload)
+        console.log('hashSeed', hashSeed);
+        await this.wallet.signAndSubmitTransaction(playerAddress, payload).then(console.log);
+    }
+
+    async SetClientSeed(playerAddress, gameId, seed) {
+        console.log('seed', gameId, seed);
+        const payload = {
+            type: "entry_function_payload",
+            function: `${this.address}::Casino::set_client_seed`,
+            type_arguments: [],
+            arguments: [gameId.toString(), seed]
+        };
+        await this.wallet.signAndSubmitTransaction(playerAddress, payload).then(console.log);
+    }
+
+    async getGameState(gameId) {
+        console.log(await aptos.client.getAccount(this.address));
+
+    }
+
+    async subscribeOnEvents(sender, eventHandleStruct, field, fromLast, callback) {
+        let from = 0;
+        if (fromLast) {
+            const lastEvent = await aptos.getEvent(this.address, this.address, eventHandleStruct, field, 0, 1)
+                .catch(() => {
+                    return null;
+                });
+            if (lastEvent) {
+                from = lastEvent["sequence_number"];
+            }
+        }
+        const loop = async () => {
+            const lastEvents = await aptos.getEvent(this.address, this.address, eventHandleStruct, field, from, 25)
+                .catch(() => {
+                    return null;
+                });
+            if (lastEvents) {
+                for (let i = 0; i < lastEvents.length; i++) {
+                    const event = lastEvents[i];
+                    callback(event);
+                    if (from === Number(event.sequence_number)) {
+                        from += 1;
+                    }
+                }
+                setTimeout(loop, 1000/*50*/);
+            } else {
+                setTimeout(loop, 5000);
+            }
+        };
+
+        setTimeout(loop, 100);
+    }
+
+    async onStartGame(eventData) {
+        console.log("onStartGame", eventData);
+
+        // if (eventData.data["player"] !== this.address) {
+        //     return;
+        // }
+
+        await this.eventHandler.onStartGame(eventData);
+    }
+
+    async onInitedBackendSeedHashes(eventData) {
+        console.log("onInitedBackendSeedHashes", eventData);
+
+        // if (eventData.data["player"] !== this.address) {
+        //     return;
+        // }
+        await this.eventHandler.onInitedBackendSeedHashes(eventData);
+    }
+
+    async onInitedBackendSeed(eventData) {
+        console.log("onInitedBackendSeed", eventData);
+        // if (eventData.data["player"] !== this.address) {
+        //     return;
+        // }
+        await this.eventHandler.onInitedBackendSeed(eventData);
+    }
+
+    async onInitedClientSeed(eventData) {
+        console.log("onInitedClientSeed", eventData);
+        // if (eventData.data["player"] !== this.address) {
+        //     return;
+        // }
+        await this.eventHandler.onInitedClientSeed(eventData);
+    }
+
+    async onCompletedGameEvent(eventData) {
+        console.log("onCompletedGameEvent", eventData);
+        // if (eventData.data["player"] !== this.address) {
+        //     return;
+        // }
+        await this.eventHandler.onCompletedGameEvent(eventData);
+        if (this.gameCompleateHandler) {
+            this.gameCompleateHandler(eventData);
+        }
+    }
+
+    handleEvents() {
+        this.subscribeOnEvents(this.address, "Casino::EventsStore",
+            "start_game_event", false, this.onStartGame)
+            .catch(console.error);
+        this.subscribeOnEvents(this.address, "Casino::EventsStore",
+            "inited_backend_seed_hashes_event", false, this.onInitedBackendSeedHashes)
+            .catch(console.error);
+        this.subscribeOnEvents(this.address, "Casino::EventsStore",
+            "inited_backend_seed_event", false, this.onInitedBackendSeed)
+            .catch(console.error);
+        this.subscribeOnEvents(this.address, "Casino::EventsStore",
+            "inited_client_seed_event", false, this.onInitedClientSeed)
+            .catch(console.error);
+        this.subscribeOnEvents(this.address, "Casino::EventsStore",
+            "completed_game_event", false, this.onCompletedGameEvent)
+            .catch(console.error);
+    }
+}
+
+export default Contract
---
+++ src/utils/blockchain/wallet.ts
@@ -0,0 +1,44 @@
+import aptos from "src/utils/blockchain/aptos";
+
+class Wallet {
+
+    constructor() {
+        if (window.pontem) {
+            this.wallet = window.pontem;
+        } else {
+            this.wallet = null;
+            throw Error("aptos wallet extension not found");
+        }
+    }
+
+    async connect() {
+        return new Promise((resolve, reject) => {
+            this.wallet.connect().then((response) => {
+                console.log('response', response);
+                this.address = response.address;
+                resolve(response);
+            }).catch((err) => {
+                console.error(err);
+                reject(err);
+            });
+        });
+    }
+
+    async getAccount() {
+        return await this.wallet.account();
+    }
+
+    async signAndSubmitTransaction(sender, payload) {
+        const options = {
+            max_gas_amount: "10000",
+            gas_unit_price: "100",
+            expiration_timestamp_secs: new Date().getTime().toString(),
+        };
+        return await this.wallet.signAndSubmit(payload, options).then(res => {
+            return res.result.hash;
+        });
+    }
+}
+
+export default Wallet
+
